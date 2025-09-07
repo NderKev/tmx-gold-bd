083868696e46dd60a98d78dc9b16c03f68b35779
NderKev <nderebakelvin@gmail.com>
2025-09-07 10:08:56 +0000

    server-remote-updates

--- migrations/user_otp.js
+++ migrations/user_otp.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
     return Promise.all([
-        knex.schema.createTable('user_otp', function (table) {
+        knex.schema.createTable('user_otps', function (table) {
             table.increments();
             table.string('email').index().references('email').inTable('users').onDelete('restrict').onUpdate('cascade');
             table.text('otp');
@@ -13,6 +13,6 @@ exports.up = function(knex) {
   //Rollback migration
   exports.down = function(knex) {
     return Promise.all([
-        knex.schema.dropTable('user_otp')
+        knex.schema.dropTable('user_otps')
     ])
   };
