5a2a9ae661abd281d66eeb4334412625ef8b3fdf
NderKev <nderebakelvin@gmail.com>
2025-09-22 21:28:52 +0300

    fix-admin-auth-21

--- middleware/checkAdmin.js
+++ middleware/checkAdmin.js
@@ -1,5 +1,5 @@
 function checkAdmin(req, res, next) {
-  if (!req.session.user || req.session.user.role !== 'customer') {
+  if (!req.session.user || req.session.user.role !== 'admin') {
     return res.status(403).send("Access denied"); 
   }
   next();
