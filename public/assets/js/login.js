3c97ce4dcfe13faed601b35360986eb1273aa7f4
NderKev <nderebakelvin@gmail.com>
2026-03-06 09:18:27 +0300

    fix-login-error

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,7 +1,8 @@
 $("#btnLogin").click(function (e) {
   e.preventDefault();
 
-  const AUTH_BACKEND_URL = "https://tmxgoldcoin.co";
+  const AUTH_BACKEND_URL = "https://tmxgoldcoin.co/tmxGold/v1"
+  // "https://tmxgoldcoin.co";
   
 
   const $btn = $("#btnLogin");
